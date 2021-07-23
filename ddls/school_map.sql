471fd400a3bbfeb1d0f702d32804c570526fabfa
akash <akashaupb@gmail.com>
2021-07-23 15:38:42 +0530

    changed a typo

--- ddls/school_map.sql
+++ ddls/school_map.sql
@@ -1,7 +1,7 @@
 CREATE TABLE SCHOOL_MAP(
 id INT NOT NULL,
 school_name VARCHAR(255) NOT NULL,
-adress varchar(255) NOT NULL,
+address varchar(255) NOT NULL,
 email_id varchar(255) NOT NULL ,
 password varchar(255) NOT NULL,
 primary key(id,school_name)
