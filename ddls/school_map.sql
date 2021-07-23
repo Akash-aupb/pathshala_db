8cec40becfa93fa0a764dba43758b76722940f3f
akash <akashaupb@gmail.com>
2021-07-23 13:53:35 +0530

    added school_map table

---
+++ ddls/school_map.sql
@@ -0,0 +1,8 @@
+CREATE TABLE SCHOOL_MAP(
+id INT NOT NULL,
+school_name VARCHAR(255) NOT NULL,
+adress varchar(255) NOT NULL,
+email_id varchar(255) NOT NULL ,
+password varchar(255) NOT NULL,
+primary key(id,school_name)
+)
